abd6e79f5cbddd2fe7717eaddcaa556324b4939a
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-12-06 23:51:07 +0000

    TrueFalseQuestion Class

---
+++ src/main/java/TrueFalseQuestion.java
@@ -0,0 +1,37 @@
+public class TrueFalseQuestion extends Questions {
+
+    public TrueFalseQuestion(String question, String correctAnswer, Difficulty difficultyLevel) {
+        super(question,correctAnswer, difficultyLevel);
+
+    }
+
+    @Override
+    public String displayQuestion() {
+        return  "True or False?";
+    }
+
+    @Override
+    public boolean checkAnswer(String userAnswer) {
+        if (userAnswer == null) {
+            return false;
+        }
+
+        String user = userAnswer.trim();
+        String correct = correctAnswer.trim();
+
+        // 1. Check full text: "True" or "False"
+        if (user.equalsIgnoreCase(correct)) {
+            return true;
+        }
+
+        // 2. Check letter: "T" or "F"
+        if (user.length() == 1) {
+            char Char = Character.toUpperCase(user.charAt(0));
+            char correctChar = Character.toUpperCase(correct.charAt(0));
+
+            return Char == correctChar;
+        }
+
+        return false;
+    }
+}
